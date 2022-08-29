3ea2f761674b0054c0a455537051091ec1ee5eef
MariamAmr01 <a.mariamgharieb@gmail.com>
2022-08-29 13:24:58 +0200

    m

---
+++ test/java/defs/filter.java
@@ -0,0 +1,26 @@
+package defs;
+
+import io.cucumber.java.en.And;
+import io.cucumber.java.en.Then;
+import io.cucumber.java.en.When;
+
+public class filter {
+    @And("the page for display tools is opened")
+    public void thePageForDisplayToolsIsOpened() {
+        
+    }
+
+    @When("the user requests to filter displayed tools by category {string}")
+    public void theUserRequestsToFilterDisplayedToolsByCategory(String arg0) {
+        
+    }
+
+    @Then("the system shows a list of tools that belong to this category as follows:")
+    public void theSystemShowsAListOfToolsThatBelongToThisCategoryAsFollows() {
+        
+    }
+
+    @Then("The system will respond with a message : {string}")
+    public void theSystemWillRespondWithAMessage(String arg0) {
+    }
+}
